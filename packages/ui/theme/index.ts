08773d96799e0a2b1edd7d8562a15435b4c0df3c
pleleu-dev <pleleu.dev@gmail.com>
2024-09-07 18:23:58 +0200

    Improve theme

--- packages/ui/theme/index.ts
+++ packages/ui/theme/index.ts
@@ -1,10 +1,12 @@
 import fonts from "./fonts/index.module.css";
-import colors from "./tokens/index.module.css";
 import global from "./global/index.module.css";
+import icons from "./icons/index.module.css";
+import colors from "./tokens/index.module.css";
 
 const theme = {
 	colors,
 	fonts,
+	icons,
 	global,
 };
 
